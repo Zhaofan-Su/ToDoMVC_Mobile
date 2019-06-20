bc178880b61cffadeb558873f6a6870207f1c774
ZhaoFan-Su <1015655239@qq.com>
2019-06-20 20:49:21 +0800

    Script design course, final project

---
+++ model.js
@@ -0,0 +1,17 @@
+// 引入model层
+window.model = {
+  data: {
+    todos: [
+      // 存储示例：
+      // {
+      //   time: 2019-06-18 0:00,
+      //   content: 'this is a example',
+      //   completed: false
+      // }
+    ],
+    content: '',
+    filter: 'All'
+  },
+  TOKEN: 'todos'
+
+}
